c1c8b8eb13917a9a5a11c0bbc4facb6f14833b78
unknown <emanuele.zini11@gmail.com>
2022-04-20 09:36:18 +0200

    feat: added different URI for token support

--- contracts/Tokens.sol
+++ contracts/Tokens.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "./ERC1155Custom.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "hardhat/console.sol";
 
-contract Tokens is ERC1155, AccessControl {
+contract Tokens is ERC1155Custom, AccessControl {
     uint256 public constant DOUBLOONS = 0;
     uint256 public constant ASTEROIDS = 1;
     uint256 public constant VE_ASTEROIDS = 2;
@@ -19,8 +19,9 @@ contract Tokens is ERC1155, AccessControl {
         keccak256("ASTEROIDS_MINTER_ROLE");
     bytes32 public constant ASTEROIDS_BURNER_ROLE =
         keccak256("ASTEROIDS_BURNER_ROLE");
+    bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
 
-    constructor() ERC1155("") {
+    constructor() {
         _mint(msg.sender, DOUBLOONS, 1000000 * (10**18), "");
         _mint(msg.sender, ASTEROIDS, 100 * (10**18), "");
 
@@ -31,10 +32,17 @@ contract Tokens is ERC1155, AccessControl {
         public
         view
         virtual
-        override(ERC1155, AccessControl)
+        override(ERC1155Custom, AccessControl)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+
+    function setURI(string memory newuri, uint256 id)
+        public
+        onlyRole(URI_SETTER_ROLE)
+    {
+        _setURI(newuri, id);
     }
 
     function mintDoubloons(address to, uint256 amount)
