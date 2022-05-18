f805cef252fd1e15a39bb114667530a13d693f3a
unknown <emanuele.zini11@gmail.com>
2022-05-18 20:18:22 +0200

    feat: added deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,23 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+  const addresses = await ethers.getSigners();
+  const owner = await addresses[0].getAddress();
+
   const SpacePiratesTokens = await ethers.getContractFactory(
     "SpacePiratesTokens"
   );
   const SpacePiratesStaking = await ethers.getContractFactory(
     "SpacePiratesStaking"
+  );
+  const SpacePiratesFactory = await ethers.getContractFactory(
+    "SpacePiratesFactory"
+  );
+  const SpacePiratesRouter = await ethers.getContractFactory(
+    "SpacePiratesRouter"
+  );
+  const SpacePiratesMasterChef = await ethers.getContractFactory(
+    "SpacePiratesMasterChef"
   );
 
   console.log("\nDeploying contracts...");
@@ -22,6 +34,35 @@ async function main() {
   console.log(
     "\nSpace Pirates Staking deployed to:",
     spacePiratesStaking.address
+  );
+
+  const spacePiratesFactory = await SpacePiratesFactory.deploy(
+    spacePiratesTokens.address
+  );
+  console.log(
+    "\nSpace Pirates Factory deployed to:",
+    spacePiratesFactory.address
+  );
+
+  const spacePiratesRouter = await SpacePiratesRouter.deploy(
+    spacePiratesFactory.address,
+    spacePiratesTokens.address
+  );
+  console.log(
+    "\nSpace Pirates Router deployed to:",
+    spacePiratesRouter.address
+  );
+
+  const spacePiratesMasterChef = await SpacePiratesMasterChef.deploy(
+    spacePiratesTokens.address,
+    owner,
+    "0x0000000000000000000000000000000000000000",
+    100,
+    0
+  );
+  console.log(
+    "\nSpace Pirates MasterChef deployed to:",
+    spacePiratesMasterChef.address
   );
 }
 
