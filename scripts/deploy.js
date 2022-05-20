68b5fbd3e3bac5220675d99c42cf59373ca6d224
unknown <emanuele.zini11@gmail.com>
2022-05-20 18:35:57 +0200

    feat: added split contract to deploy and contracts setup

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,65 @@
 const { ethers } = require("hardhat");
+const data = require("./data.json");
 
 async function main() {
-  const SpacePiratesTokens = await ethers.getContractFactory(
-    "SpacePiratesTokens"
-  );
-  const SpacePiratesStaking = await ethers.getContractFactory(
+  /* Contracts loading */
+  const TokensContract = await ethers.getContractFactory("SpacePiratesTokens");
+  const StakingContract = await ethers.getContractFactory(
     "SpacePiratesStaking"
   );
+  const SplitContract = await ethers.getContractFactory(
+    "AsteroidsSplitContract"
+  );
 
+  /* Contracts deploy */
   console.log("\nDeploying contracts...");
+  const tokensContract = await TokensContract.deploy();
+  console.log("Space Pirates Tokens deployed to:", tokensContract.address);
+  const stakingContract = await StakingContract.deploy(tokensContract.address);
+  console.log("Space Pirates Staking deployed to:", stakingContract.address);
+  const splitContract = await SplitContract.deploy(tokensContract.address);
+  console.log("Asteroids Split Contract deployed to:", splitContract.address);
 
-  const spacePiratesTokens = await SpacePiratesTokens.deploy();
-  console.log(
-    "\nSpace Pirates Tokens deployed to:",
-    spacePiratesTokens.address
+  /* Contracts setup*/
+  console.log("\nContracts setup...");
+  console.log("  staking contract setup");
+  await tokensContract.grantRole(data.mint.doubloons, stakingContract.address);
+  console.log("    granted doubloons mint role");
+  await stakingContract.createStakingPool(
+    data.ids.doubloons,
+    data.ids.doubloons,
+    ethers.BigNumber.from("2000000000000000000"),
+    0
   );
+  console.log("    created doubloons staking pool");
+  await stakingContract.createStakingPool(
+    data.ids.stkAsteroids,
+    data.ids.doubloons,
+    ethers.BigNumber.from("2000000000000000000"),
+    0
+  );
+  console.log("    created stk-asteroids staking pool");
 
-  const spacePiratesStaking = await SpacePiratesStaking.deploy(
-    spacePiratesTokens.address
+  console.log("  asteroids split contract setup");
+  await tokensContract.grantMultiRole(
+    [
+      data.mint.asteroids,
+      data.mint.veAsteroids,
+      data.mint.stkAsteroids,
+      data.burn.asteroids,
+      data.burn.veAsteroids,
+      data.burn.stkAsteroids,
+    ],
+    [
+      splitContract.address,
+      splitContract.address,
+      splitContract.address,
+      splitContract.address,
+      splitContract.address,
+      splitContract.address,
+    ]
   );
-  console.log(
-    "\nSpace Pirates Staking deployed to:",
-    spacePiratesStaking.address
-  );
+  console.log("    granted mint & burn role to the split contract");
 }
 
 main()
