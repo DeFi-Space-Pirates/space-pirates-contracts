8b9ee9a7dfd5d88f65d55e4c6de4421e7e2faad4
yuripaoloni <yuri.paoloni3@gmail.com>
2022-06-24 13:10:40 +0200

    perf: erc20 transafer, constructor

--- contracts/SpacePiratesWrapped.sol
+++ contracts/SpacePiratesWrapped.sol
@@ -1,62 +1,80 @@
 // SPDX-License-Identifier: unlicense
 pragma solidity ^0.8.0;
+
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./ERC1155Custom.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract SpacePiratesWrapped is Ownable, ERC1155Custom {
-    address[] idSupport;
-
-    mapping(address => TokenInfo) tokens;
-    mapping(address => mapping(address => bool)) approvals;
+contract SpacePiratesWrapped is Ownable {
 
     struct TokenInfo {
-        uint256 tokenId;
+        uint256 id;
         string name;
         bool exists;
         uint256 totalSupply;
-        mapping(address => uint256) balances;
     }
 
-    function addNewToken(address addr, string memory name) external onlyOwner {
-        require(tokens[addr].exists == false);
-        tokens[addr] = new TokenInfo(idSupport.length + 1, name, true, 0);
-        idSupport.push(addr);
+    address[] supportedTokens;
+    mapping(address => TokenInfo) public tokens;
+
+    mapping(address => mapping(address => bool)) public approvals;
+
+    mapping(address => mapping(address => uint256)) public balances;
+
+    constructor() {
+        tokens[address(0)] = TokenInfo(0, "spaceETH", true, 0);
+        supportedTokens.push(address(0));
+    }
+
+    function addNewToken(address _addr, string memory name) external onlyOwner {
+        require(tokens[_addr].exists == false, "SpacePiratesWrapped: TOKEN ALREADY EXISTS");
+        
+        tokens[_addr] = TokenInfo(supportedTokens.length + 1, name, true, 0);
+        supportedTokens.push(_addr);
     }
 
     //if necessary we can modify other fields of token info
-    function updateToken(address addr, bool choose) external onlyOwner {
-        tokens[addr].exists = choose;
+    //? is actually needed this function?
+    function updateToken(address _addr, bool _exists) external onlyOwner {
+        tokens[_addr].exists = _exists;
+
+        // code to update the other fields
     }
 
-    function mintToken(address addr, uint256 amount) public {
-        require(tokens[addr].exists == true);
-        //TODO: erc20 transfer
-        tokens[addr].totalSupply += amount;
-        tokens[addr].balances[msg.sender] += amount;
+    function mintTokens(address _addr, uint256 _amount) public {
+        require(tokens[_addr].exists == true, "SpacePiratesWrapped: TOKEN DOES NOT EXISTS");
+        
+        IERC20(_addr).approve(address(this), _amount); 
+        IERC20(_addr).transferFrom(msg.sender, address(this), _amount);
+
+        tokens[_addr].totalSupply += _amount;
+        balances[_addr][msg.sender] += _amount;
     }
 
-    function redeemToken(address addr, uint256 amount) public {
-        require(tokens[addr].exists == true);
-        //TODO: erc20 transfer
-        tokens[addr].totalSupply -= amount;
-        tokens[addr].balances[msg.sender] -= amount;
+    function redeemTokens(address _addr, uint256 _amount) public {
+        require(tokens[_addr].exists == true, "SpacePiratesWrapped: TOKEN DOES NOT EXISTS");
+        require(balances[_addr][msg.sender] >= _amount, "SpacePiratesWrapped: NOT ENOUGH TOKENS");
+        
+        IERC20(_addr).transferFrom(address(this), msg.sender, _amount);
+
+        tokens[_addr].totalSupply -= _amount;
+        balances[_addr][msg.sender] -= _amount;
     }
 
     receive() external payable {
         ethDeposit();
     }
 
-    function addToken(address _addr) external payable receive {
-        nativeTokenDeposit();
+    function ethDeposit() public payable {
+        mintTokens(address(0), msg.value);
     }
 
-    function nativeTokenDeposit() public payable {
-        _mint(msg.sender, 0, msg.value, "");
+    function ethWithdraw(uint256 _amount) public {
+        redeemTokens(address(0), _amount);
+
+        (bool success, ) = msg.sender.call{value: _amount}("");
+        require(success, "SpacePiratesWrapped: WITHDRAW FAILED");
     }
 
-    function nativeTokenWithdraw(uint256 amount) public {
-        _burn(msg.sender, 0, amount);
-        (bool success, ) = msg.sender.call{value: amount}("");
-        require(success, "withdrawal failed");
-    }
+    // TODO
+    function transferToken() public {}
 }
