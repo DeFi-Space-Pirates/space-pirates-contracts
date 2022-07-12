0fefe3baca3ecafb28b533fccf3a70d665af9a74
unknown <emanuele.zini11@gmail.com>
2022-07-12 19:22:15 +0200

    feat: updated the contract interface

--- contracts/interfaces/ISpacePiratesItemsMarketPlace.sol
+++ contracts/interfaces/ISpacePiratesItemsMarketPlace.sol
@@ -7,13 +7,23 @@ pragma solidity ^0.8.0;
  */
 
 interface ISpacePiratesItemsMarketPlace {
-    event AddItems(uint256 indexed itemId, uint128 paymentId, uint128 price, uint64 itemQuantity, uint64 saleEnd, uint64 available);
-    event BuyItem(uint256 indexed itemId, uint128 paymentId, uint128 price, uint64 quantity);
+    event AddItems(uint256[] indexed itemsIds, uint256[] itemsQuantities, uint128 paymentId, uint128 price, uint120 saleEnd, uint120 available, uint16 maxBuyPerAddress);
+    event BuyItem(uint256[] indexed itemsIds, uint128 paymentId, uint128 price, uint64 quantity);
 
-    function tokenContract() external view returns(address);
+    function tokenContract() external view returns(address tokenContract);
+
     function itemsOnSale(uint256 index) external view returns(uint256 id);
-    function sales(uint256 id, uint256 index) external view returns (uint128 paymentId, uint128 price, uint64 itemQuantity, uint64 saleEnd, uint64 available);
+    function itemsOnSaleArray() external view returns (uint256[] memory ids);
+
+    function saleItemsIds(uint256 saleIndex) external view returns (uint256[] memory ids);
+    function saleItemsQuantities(uint256 saleIndex) external view returns (uint256[] memory quantities);
     
+    function saleIndexes(uint256 id, uint256 index) external view returns (uint256 saleIndex);
+    function salesIndexesFromId(uint256 id) external view returns (uint256[] memory saleIndexes);
+
+    function sales(uint256 saleIndex) external view returns (uint128 paymentId, uint128 price, uint120 saleEnd, uint120 available, uint16 maxBuyPerAddress);
+    function salesAmount() external view returns (uint256 amount);
+
     function createSale(uint256 itemId, uint128 paymentId, uint128 price, uint64 itemQuantity, uint64 saleEnd, uint64 available ) external;
     
     function buyItem(uint256 itemId, uint256 saleIndex, uint64 quantity) external;
